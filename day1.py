7ef46bd442adbf700c013708be97353699fa67d6
radagast <noname@notexist.com>
2025-05-11 07:36:54 +0200

    refactoring

--- day1.py
+++ day1.py
@@ -1,128 +1,54 @@
 moves = ["R5", "L5", "R5", "R3"]
 
+dirs = [
+    (-1, 0),  # N
+    (0, 1),  # E
+    (1, 0),  # S
+    (0, -1),  # W
+]
+
 
 def distance(moves):
-    x, y, dir = 0, 0, "N"
+    x, y, dir = 0, 0, 0
     for move in moves:
         turn = move[0]
         steps = int(move[1:])
-        if turn == "R":
-            if dir == "N":
-                dir = "E"
-                x += steps
-            elif dir == "E":
-                dir = "S"
-                y += steps
-            elif dir == "S":
-                dir = "W"
-                x -= steps
-            else:  # W
-                dir = "N"
-                y -= steps
-        if turn == "L":
-            if dir == "N":
-                dir = "W"
-                x -= steps
-            elif dir == "E":
-                dir = "N"
-                y -= steps
-            elif dir == "S":
-                dir = "E"
-                x += steps
-            else:  # W
-                dir = "S"
-                y += steps
+        dir = (dir + 1) % 4 if turn == "R" else (dir - 1) % 4
+        dx, dy = dirs[dir]
+        x += dx * steps
+        y += dy * steps
 
     return abs(x) + abs(y)
 
 
 def first_visited_twice_distance(moves):
-    x, y, dir = 0, 0, "N"
+    x, y, dir = 0, 0, 0
     visited = set()
-    visited.add((x, y))
-
+    visited.add((0, 0))
     for move in moves:
         turn = move[0]
         steps = int(move[1:])
-        if turn == "R":
-            if dir == "N":
-                dir = "E"
-                for d in range(1, steps + 1):
-                    p = (x + d, y)
-                    if p in visited:
-                        return abs(p[0]) + abs(p[1])
-                    visited.add(p)
-                x += steps
-            elif dir == "E":
-                dir = "S"
-                for d in range(1, steps + 1):
-                    p = (x, y + d)
-                    if p in visited:
-                        return abs(p[0]) + abs(p[1])
-                    visited.add(p)
-                y += steps
-            elif dir == "S":
-                dir = "W"
-                for d in range(1, steps + 1):
-                    p = (x - d, y)
-                    if p in visited:
-                        return abs(p[0]) + abs(p[1])
-                    visited.add(p)
-                x -= steps
-            else:  # W
-                dir = "N"
-                for d in range(1, steps + 1):
-                    p = (x, y - d)
-                    if p in visited:
-                        return abs(p[0]) + abs(p[1])
-                    visited.add(p)
-                y -= steps
-        if turn == "L":
-            if dir == "N":
-                dir = "W"
-                for d in range(1, steps + 1):
-                    p = (x - d, y)
-                    if p in visited:
-                        return abs(p[0]) + abs(p[1])
-                    visited.add(p)
-                x -= steps
-            elif dir == "E":
-                dir = "N"
-                for d in range(1, steps + 1):
-                    p = (x, y - d)
-                    if p in visited:
-                        return abs(p[0]) + abs(p[1])
-                    visited.add(p)
-                y -= steps
-            elif dir == "S":
-                dir = "E"
-                for d in range(1, steps + 1):
-                    p = (x + d, y)
-                    if p in visited:
-                        return abs(p[0]) + abs(p[1])
-                    visited.add(p)
-                x += steps
-            else:  # W
-                dir = "S"
-                for d in range(1, steps + 1):
-                    p = (x, y + d)
-                    if p in visited:
-                        return abs(p[0]) + abs(p[1])
-                    visited.add(p)
-                y += steps
+        dir = (dir + 1) % 4 if turn == "R" else (dir - 1) % 4
+        dx, dy = dirs[dir]
+        for step in range(1, steps + 1):
+            x += dx
+            y += dy
+            if (x, y) in visited:
+                return abs(x) + abs(y)
+            visited.add((x, y))
 
-    raise ValueError("Did not visit any point twice")
+    raise ValueError("ERROR!")
+
+
+with open("./resources/day1.txt") as f:
+    moves = f.read().strip().split(", ")
 
 
 def part1():
-    with open("./resources/day1.txt") as f:
-        moves = f.read().strip().split(", ")
     print(distance(moves))
 
 
 def part2():
-    with open("./resources/day1.txt") as f:
-        moves = f.read().strip().split(", ")
     print(first_visited_twice_distance(moves))
 
 
