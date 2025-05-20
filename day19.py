d621916448b322b1dfa7ae5a4bb17eff28152f6d
Radagast <rslabon@gmail.com>
2025-05-20 15:03:32 +0200

    in progress

--- day19.py
+++ day19.py
@@ -3,6 +3,10 @@ class Node:
         self.id = id
         self.value = value
         self.next = None
+        self.prev = None
+
+    def __repr__(self):
+        return f"#{self.id} [{self.value}]"
 
 
 def part1():
@@ -25,25 +29,32 @@ def part1():
 
 
 def part2():
-    elfs = dict()
+    root = Node(1, 1)
+    current = root
+    nodes = dict()
     i = 1
-    while i <= 5:
-        elfs[i] = 1
+    while i < 5:
+        nodes[current.id] = current
+        node = Node(i + 1, 1)
+        current.next = node
+        node.prev = current
+        current = current.next
         i += 1
 
-    current = 1
-    while len(elfs) > 1:
-        size = len(elfs)
-        if size % 2 == 0:
-            size += 1
-        i = current + (size // 2)
-        elfs[current] += elfs[i]
-        del elfs[i]
-        current = (current + 1) % (1 + len(elfs))
-        while current not in elfs:
-            current = (current + 1) % (1 + len(elfs))
+    nodes[current.id] = current
+    current.next = root
+    root.prev = current
 
-    print(elfs)
+    current = root
+    while current.next != current:
+        size = len(nodes)
+        opposite_id = current.id + (size // 2)
+        opposite_node = nodes[opposite_id]
+        current.value += opposite_node.value
+        opposite_node.prev.next = opposite_node.next
+        current = current.next
+
+    print(current.id, current.value)
 
 
 # part1()
